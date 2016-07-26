2d0566bf6de0451f5a4e9ac9af8583cef224a382
twatterz <twatters@uccs.edu>
2016-07-26 10:08:26 -0600

    added in hulk ryu with jquery animation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+//ryu animation
 	$('.ryu').mouseenter(function () {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
@@ -19,7 +19,7 @@ $(document).ready(function(){
 			500,
 			function() {
 				$(this).hide();
-				$(this).css('left', '52rem');
+				$(this).css('left', '50.2rem');
 			});
 	})
 
@@ -37,6 +37,46 @@ $(document).ready(function(){
 		if (event.keyCode=88) {
 			$('.ryu-cool').hide();
 			$('ryu-still, .ryu-ready').show();
+		}
+	});
+//hulk ryu animations
+	$('.hulk-ryu').mouseenter(function(){
+		$('.hulk-ryu-still').hide();
+		$('.hulk-ryu-ready').show();
+	})
+
+	.mouseleave(function(){
+		$('.hulk-ryu-ready').hide();
+		$('.hulk-ryu-still').show();
+	})
+
+	.mousedown(function() {
+		playHadouken();
+		$('.hulk-ryu-ready').hide();
+		$('.hulk-ryu-throwing').show();
+		$('.hulk-hadouken').finish().show().animate(
+			{'right': '102rem'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('right', '40rem');
+			});
+	})
+
+	.mouseup(function() {
+		$('.hulk-ryu-throwing').hide();
+		$('.hulk-ryu-ready').show();
+	});
+
+	$(document).keydown(function(event) {
+		if (event.keyCode=86) {
+			$('.hulk-ryu-still, .hulk-ryu-ready').hide();
+			$('.hulk-ryu-cool').show();
+		}
+	}) .keyup(function(event) {
+		if (event.keyCode=88) {
+			$('.hulk-ryu-cool').hide();
+			$('.hulk-ryu-ready').show();
 		}
 	});
 });
